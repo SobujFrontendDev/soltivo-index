1da9aca8aec5f7ebe2acdb3d9dc39c3a68c72096
masud rana sobuj <sobuj.frontend.dev22@gmail.com>
2023-03-13 14:25:27 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(function(){
+    $('.banner-inner').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay:true,
+        nextArrow:'.next',
+        prevArrow:'.prev',
+      });
+});
+
+
+
+
+
+// $(function(){
+//     $('.study-inner').slick({
+//         dots: false,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 3,
+//         slidesToScroll:1,
+//         adaptiveHeight: true,
+//         autoplay:true,
+//         nextArrow:'.next',
+//         prevArrow:'.prev',
+//       });
+// });
